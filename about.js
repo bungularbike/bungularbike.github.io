6d0aab4c9e4a4ffb8f6610adafe24de2ea5278b7
bungularbike <rongjackmiles@gmail.com>
2019-08-30 22:46:48 +0800

    D

--- about.js
+++ about.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
 	$("#fade").animate({ opacity: 0 }, 1000, function() { $("#fade").remove() });
-	setTimeout(function() { $("nav, .panel, .card, .panel-heading").animate({ opacity: 1 }, 1000); setTimeout(slideshow, 5000) }, 1000);
+	setTimeout(function() { $("nav, .panel, .card, .panel-heading").animate({ opacity: 1 }, 1000) }, 1000);
 
 });
